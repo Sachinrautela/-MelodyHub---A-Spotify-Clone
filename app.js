b71c7cd6080210e5f31402b3f06b07f2c3d23589
Sachin Rautela <rautelasachin26@gmail.com>
2025-03-06 12:22:53 +0530

    Add files via upload
    MelodyHub is a Spotify-inspired music streaming web application built using HTML, CSS, and JavaScript. It features a modern and responsive UI with smooth animations, playlist management, and interactive music controls. This project demonstrates frontend development skills with an emphasis on CSS styling, media queries, and UI/UX design.
    
    🔹 Tech Stack: HTML, CSS, JavaScript
    🔹 Features: Play/Pause songs, Custom UI, Responsive Design, Playlist UI
    
    🚀 Future Enhancements: Backend integration with Node.js, Express, and MongoDB for user authentication and real-time music streaming.

---
+++ app.js
@@ -0,0 +1,23 @@
+// let smallImages=document.getElementsByClassName("card-img");
+
+// for(let i=0;i<smallImages.length;i++){
+//     smallImages[i].src="assets/card1img.jpeg"
+//     console.log(`value of image no.${i} is changed.`); 
+// }
+
+// console.dir(document.querySelector("p"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".card-img"));
+// console.dir(document.querySelectorAll("div p"));
+
+// let para=document.querySelectorAll(".card p");
+
+// for(let i=0;i<para.length;i++){
+//     para[i].style.color="green";
+//     para[i].style.backgroundColor="yellow";
+// }
+
+
+// for(para of paras){
+//     para[i].style.color="green";
+// }
